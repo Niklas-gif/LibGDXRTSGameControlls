0fccc06d01f03b4973b492b8c45984f7919beacb
niklas-gif <niklas.demel1@gmail.com>
2023-01-16 17:06:16 +0100

    Worked on scroll & resize function.

--- core/src/com/mygdx/libgdxrtsgamecontrols/GameScreen.java
+++ core/src/com/mygdx/libgdxrtsgamecontrols/GameScreen.java
@@ -8,11 +8,16 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GameScreen implements Screen {
     private final RTSGame game ;
     private OrthographicCamera camera;
     private Texture map;
+    private Viewport viewport;
+    private int width;
+    private int height;
     private Stage borders;
 
     public GameScreen(RTSGame game){
@@ -20,9 +25,13 @@ public class GameScreen implements Screen {
         map = new Texture("Test_Map.png");
         camera = new OrthographicCamera();
         camera.setToOrtho(false, GlobalVariables.SCREEN_WIDTH,GlobalVariables.SCREEN_HEIGHT);
+        viewport = new FitViewport(GlobalVariables.SCREEN_WIDTH,GlobalVariables.SCREEN_HEIGHT,camera);
+        viewport.apply();
     }
     @Override
     public void show() {
+        width = GlobalVariables.SCREEN_WIDTH;
+        height = GlobalVariables.SCREEN_HEIGHT;
 
     }
 
@@ -39,7 +48,10 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        System.out.println("width: " + width + " height: " + height );
+        this.width = width;
+        this.height = height;
+        viewport.update(width,height);
     }
 
     @Override
@@ -63,14 +75,15 @@ public class GameScreen implements Screen {
         game.dispose();
     }
 
-    private void scroll(){ //Test Scroll Function
+    private void scroll(){ //Test Scroll Function TODO: Should not depend on the Screen size
         if(Gdx.input.isKeyPressed(Input.Keys.I) && camera.zoom < 3){
             camera.zoom += 2f * Gdx.graphics.getDeltaTime();
         }
         if(Gdx.input.isKeyPressed(Input.Keys.O) && camera.zoom > 0.5){
             camera.zoom -= 2f * Gdx.graphics.getDeltaTime();
         }
-        if(Gdx.input.getX()>=GlobalVariables.SCREEN_WIDTH-500){
+        if(Gdx.input.getX()>=width-500){
+            //System.out.println("scroll width:" + width);
             camera.position.set(camera.position.x+=5,camera.position.y,0);
         }
         if(Gdx.input.getX()<=100){
@@ -79,7 +92,7 @@ public class GameScreen implements Screen {
         if(Gdx.input.getY()<=100){
             camera.position.set(camera.position.x,camera.position.y+=5,0);
         }
-        if(Gdx.input.getY()>=GlobalVariables.SCREEN_HEIGHT-400){
+        if(Gdx.input.getY()>=height-400){
             camera.position.set(camera.position.x,camera.position.y-=5,0);
         }
     }
